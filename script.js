50d77e1b63337598a422b64b671049035fe5e2f3
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-08 20:47:27 -0500

    Fix buttons in script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-element.addEventListener("click", dnButton);
+//date night button - needs to return a random item from unordered list
+const element1 = document.getElementById("dnBtn");
+element1.addEventListener("click", dnButton);
 
 function dnButton() {
-    document.getElementById("dnBtn").innerHTML = "Click Here";
+    document.getElementById("dnBtn")
+    console.log(dnButton)
+}
+
+//netflix button - needs to connect to API Netflix Roulett to somehow return a show or movie 
+const element2 = document.getElementById("netflixBtn");
+element2.addEventListener("click", netflixButton)
+
+function netflixButton() {
+    document.getElementById("netflixBtn")
+    console.log(netflixButton)
 }
