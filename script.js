1437e2ac6ef6d7272a083027fc5cd3bfffcdb34a
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-09 20:05:18 -0500

    Added text box submission that returns the input text in the ultimateList <ul>

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function netflixButton() {
     document.getElementById("netflixBtn")
     console.log(netflixButton)
 }
+
+//drag li items to list area?
+
+//fullscreenchange possible event for when soemthing is returned from clicking dnBtn
+
+//open for API?
+
+//need to reset form once something is input
+
+//form submit
+
+const form = document.getElementById("personal-ideas") 
+form.addEventListener("submit", (addIdea) => {
+    addIdea.preventDefault()
+    console.log("submit", addIdea)
+
+}) 
+
+//input text gets added to The Ultimate List
+document.getElementById("add").onclick = function() {
+    var node = document.createElement("li")
+    var text = document.getElementById("newIdea").value;
+    var textnode = document.createTextNode(text);
+    node.appendChild(textnode);
+    document.getElementById("ultimateList").appendChild(node);
+}
