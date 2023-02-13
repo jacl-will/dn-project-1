ba12d6832de382f9f4da87b21a58ef8872c97685
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-12 21:20:35 -0500

    reformatted layout and updated html to fit new API that will be used

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Date Night is a tool that couples, friends, and families can use to decide on a 
 
 How it does this is by allowing the user to select from a basic list of ideas to add to "their" list. Users can also add their own ideas to the list by writing them out. Once the list is to their liking then they will click the button that will randomly select the date for them. 
 
-If the date selected was to have a Netflix night OR the user just wants some help selecting a show or movie to watch then they can use the button (hooked up to the Netflix Roulette API) to get an answer. 
+<!-- If the date selected was to have a Netflix night OR the user just wants some help selecting a show or movie to watch then they can use the button (hooked up to the Netflix Roulette API) to get an answer.  -->
+
+API - Bored [Link](https://www.boredapi.com/api/activity)
--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-//date night button - needs to return a random item from unordered list
-const element1 = document.getElementById("dnBtn");
-element1.addEventListener("click", dnButton);
-
-function dnButton() {
-    document.getElementById("dnBtn")
-    console.log(dnButton)
-}
-
-//netflix button - needs to connect to API Netflix Roulett to somehow return a show or movie 
-const element2 = document.getElementById("netflixBtn");
-element2.addEventListener("click", netflixButton)
-
-function netflixButton() {
-    document.getElementById("netflixBtn")
-    console.log(netflixButton)
-}
 
 //drag li items to list area? ---> check box that adds it to the ultimate list 
 
-//fullscreenchange possible event for when soemthing is returned from clicking dnBtn
-
-//open for API?
-
-//need to reset form once something is input
 
 //form submit
-const form = document.getElementById("personal-ideas") 
-form.addEventListener("submit", (addIdea) => {
-    addIdea.preventDefault()
-    // console.log("submit", addIdea)
 
+const form = document.getElementById("personal-ideas") 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    var addIdea = document.getElementById("newIdea").value
+    console.log("newIdea", addIdea)
+   
 }) 
+//need to reset form once something is input
 
 //input text gets added to The Ultimate List
 document.getElementById("add").onclick = function() {
@@ -40,3 +21,44 @@ document.getElementById("add").onclick = function() {
     node.appendChild(textnode);
     document.getElementById("ultimateList").appendChild(node);
 }
+
+//date night button - needs to return a random item from unordered list
+const element1 = document.getElementById("dnBtn");
+element1.addEventListener("click", dnButton);
+
+function dnButton() {
+    // var idea = document.getElementByElement("li")
+    document.getElementById("dnBtn")
+    // return idea[Math.floor(Math.random()*idea.length)];
+    console.log(dnButton) 
+}
+
+//changed to random activity button - needs to connect to Bored API 
+const element2 = document.getElementById("boredBtn");
+element2.addEventListener("click", boredButton)  
+
+function boredButton() {
+    document.getElementById("boredBtn")
+    console.log(boredButton)
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://www.boredapi.com/api/activity')
+    .then(res => console.log(res))
+
+    // .then(data => { 
+    //     data.activity.forEach(hobby => {
+    //         const div = document.createElement('div')
+    //         const h3 = document.createElement('h3')
+    //         h3.textContent = hobby.activity
+    //         div.append("h3")
+    //         document.querySelector("#hobby").append(div)
+    //     })    
+    // })
+})
+
+//Fetch Request
+// function getActivity() {
+//     fetch('https://www.boredapi.com/api/activity')
+// }
