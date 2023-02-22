0c332333ae1a1448e250ebb6b8c575d3eb2f6cb7
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-02-22 15:03:23 -0500

    worked on js, html, and css to add post for form, and to work through a different layout for code

--- script.js
+++ script.js
@@ -1,56 +1,147 @@
+// //loading DOM and submit form event listener
+// document.addEventListener('DOMContentLoaded', () => {
+//     getDates();
+//     document.getElementById("personalIdeas").addEventListener("submit", (event) => {
+//         event.preventDefault()
+//         let addIdea = {
+//             date: document.getElementById("newIdea").value,
+//             description: document.getElementById("description").value
+//         }
+//         console.log("event", addIdea)
+//         // add userIdea to date list
+//         let node = document.createElement("li")
+//             let text = document.getElementById("newIdea").value;
+//             var textnode = document.createTextNode(text);
+//             node.appendChild(textnode);
+//         document.getElementById('randomDate').appendChild(node)
+//        document.forms[0].reset();
+//     }) 
+// })
+
+// //fetch request
+// function getDates(){
+//     fetch('http://localhost:3000/basicIdeas')
+//     .then(res => res.json())
+//     .then(dateData => dateData.forEach(dates => renderAllDates(dates)))
+// }
+
+// //DOM Render Function ---->add way to randomly select 1 date
+// function renderAllDates(dates){
+//     //Build date
+//     let theDates = document.createElement('p')
+//     theDates.className = 'theDat'
+//     theDates.innerHTML = `
+//     <p>
+//     ${dates.dateName}
+//     </p>
+//     `
+//     //build description
+//     let theDescription = document.createElement('p')
+//     document.querySelector('.dateList').appendChild(theDates)
+
+//     //mouseover event
+//     theDates.addEventListener('mouseover', (ev) => {
+//         theDescription.innerHTML= `${dates.description}`;
+//         document.querySelector('.theDat').appendChild(theDescription) 
+//         // console.log(theDates)
+//     })
+
+//     //mouseout event
+//     theDates.addEventListener('mouseout', (ev) => theDescription.innerHTML = '') 
+
+//     //dnButton to select random date ----->currently does nothing 
+//     const rDateBtn = document.getElementById('dnBtn');
+//     rDateBtn.addEventListener('click', dnButton);
+
+//     function dnButton() {
+//         // return theDates[Math.floor(Math.random()*theDates.length)];
+//     console.log(dnButton)
+// } 
+// }
+
+
+
+
+
 //loading DOM and submit form event listener
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("personalIdeas").addEventListener("submit", (event) => {
-        event.preventDefault()
-        let addIdea = {
-            date: document.getElementById("newIdea").value,
-            description: document.getElementById("description").value
-        }
-        console.log("event", addIdea)
-        // add userIdea to date list
-        let node = document.createElement("li")
-            let text = document.getElementById("newIdea").value;
-            var textnode = document.createTextNode(text);
-            node.appendChild(textnode);
-        document.getElementById('randomDate').appendChild(node)
-       document.forms[0].reset();
-    }) 
+    getDates()
+    document.getElementById("personalIdeas").addEventListener("submit", addUserDate)
+
 })
 
-//DOM Render Function ---->add way to randomly select 1 date
-function renderAllDates(dates){
-    //Build date
-    let theDates = document.createElement('p')
-    theDates.className = 'theDat'
-    theDates.innerHTML = `
-    <p>
-    ${dates.dateName}
-    </p>
-    `
-    let theDescription = document.createElement('p')
-    document.querySelector('#randomDate').appendChild(theDates)
-
-    //mouseover event
-    theDates.addEventListener('mouseover', function(){
-        theDescription.innerText= `${dates.description}`;
-        document.querySelector('p').appendChild(theDescription)
-        // console.log(theDates)
+//form submit and post 
+function addUserDate(event) {
+    event.preventDefault()
+    const date = {
+        dateName: document.getElementById("newIdea").value,
+        description: document.getElementById("description").value
+    }
+    fetch('http://localhost:3000/basicIdeas', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(date)
     })
-
-    //mouseout event
-    theDates.addEventListener('mouseout', (ev) => theDescription.innerText = '')
+    .then(res => res.json())
+    .then(data => {
+        const dates = document.querySelector(".dateList")
+        dates.innerHTML += renderSingleDate(data)      
+        document.forms[0].reset();
+    })
 }
 
 //fetch request
 function getDates(){
-    fetch('http://localhost:3000/basic-ideas')
+    fetch('http://localhost:3000/basicIdeas')
     .then(res => res.json())
-    .then(dateData => dateData.forEach(dates => renderAllDates(dates)))
+    .then(data => {
+        const dates = document.querySelector(".dateList")
+        dates.innerHTML = renderAllDates(data)
+        addDateListeners()
+    })
 }
 
-//initial render
-function initialize() {
-    getDates()
+function addDateListeners() {
+    const dates = document.querySelector(".dateList")
+    dates.addEventListener('mouseover', mouseOver)
+    // dates.addEventListener('mouseout', mouseOut)
 }
-initialize()
+
+function mouseOver(event) {
+    // console.log(event.target.id)
+    fetch(`http://localhost:3000/basicIdeas/${event.target.id}`)
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => {
+        const description = document.querySelector(".dateCard")
+        description.innerHTML += renderDescription(data)
+    })
+}
+
+// function mouseOver(event) {
+//     const dates = document.querySelector(".dateList")
+// }
+
+function renderDescription(description) {
+   console.log(description)
+}
+
+function renderAllDates(dates) {
+    let theDescription = document.createElement('p')
+    document.querySelector('.dateList').append(dates)
+    
+    return dates.map(dn => renderSingleDate(dn)).join("")
+}
+
+function renderSingleDate(date) {
+    return `   
+    <div class="dateCard" id="${date.id}">
+            <h3 class="dateN">
+                ${date.dateName}
+            </h3>
+    </div>`
+}
 
